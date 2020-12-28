d553f813e4755c2e28723d71fe0d8cb6831ed490
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2020-12-28 17:12:40 -0500

    var names, styling
    Styling changes to the index.html template
    Variable name changes, new db created

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 # Import Dependencies 
 from flask import Flask, render_template, redirect
-import pymongo
+from flask_pymongo import PyMongo
 import scrape_mars
-
 
 # 
 app = Flask(__name__)
 
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
 # Creating a route that renders the index.html page 
 @app.route('/')
-def index():
+def home():
+
+    mars = mongo.db.mars.find_one()
+
     return render_template('index.html', mars=mars)
+
+@app.route('/scrape')
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_all()
+
+    # Update the Mongo database 
+    mongo.db.mars.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -11,9 +11,11 @@ mars = {}
 def scrape_all():
 
 # Creating a path for browser used to scrape dynamic website 
-    chrome_path = 'driver/chromedriver.exe' 
+    chrome_path = 'C:/Users/guyan/bin/chromedriver.exe' 
     executable_path = {'executable_path' : chrome_path}
     browser = Browser('chrome', **executable_path, headless=False)
+
+
 
 # Opening news section in browser
     news_url = 'https://mars.nasa.gov/news/'
@@ -23,12 +25,10 @@ def scrape_all():
     news_html = browser.html
     news = bs(news_html, 'html.parser')
 
-
-# Finding the first title and paragraph on the page and adding to dictionary 
+# Finding the first title and paragraph on the page 
     news_title = news.find_all('div', class_='content_title')[1].text
-    mars.news_title = news_title
     news_p = news.find('div', class_='article_teaser_body').text
-    mars.news_p= news_p
+    
 
 
 # Opening jet propulsion lab website
@@ -39,13 +39,12 @@ def scrape_all():
     jpl_html = browser.html
     jpl = bs(jpl_html, 'html.parser')
 
-
 # extracting the url for the featured image
     site_dir = jpl_url.split('?')[0]
     rel_image_path = jpl.find('article', class_='carousel_item')['style']
     rel_image_path = rel_image_path.split('spaceimages/')[1].split("'")[0]
     featured_image_url = site_dir+rel_image_path
-    mars.featured_image_url = featured_image_url
+    
 
 
 #Opening Mars Facts url 
@@ -56,20 +55,57 @@ def scrape_all():
     facts_html = browser.html
     mars_facts = bs(facts_html, 'html.parser')
 
-
 # Scraping the fact table and making it tidy 
+    fact_table_html = pd.read_html('https://space-facts.com/mars/')
     mars_facts_df = (fact_table_html[0].rename(columns={0:'Description', 1:'Mars'}))
     mars_facts_df.set_index("Description", inplace=True)
 # Creating an html table to render in the index.html page
     mars_facts_html = mars_facts_df.to_html()
-    mars.mars_facts_html = mars_facts_html
+    
+
 
 #Opening USGS website 
     usgs_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(usgs_url)
 
+# Converting html page to a beautiful soup object
+    usgs_html = browser.html
+    usgs = bs(usgs_html, 'html.parser')
 
+# Scraping the title and image of each hemisphere, saving each in its own dictionary, within a list
+    hemisphere_image_urls = []
+    usgs_main = 'https://astrogeology.usgs.gov'
+
+    hemispheres = usgs.find_all('div', class_='item')
+    for hemi in hemispheres:
+        sphere = {}
+
+        title = hemi.find('h3').text
+
+        image_path = hemi.find('a', class_='itemLink product-item')['href']
+        browser.visit(usgs_main + image_path)
+
+        image_html = browser.html
+        image = bs(image_html, 'html.parser')
+        img_url = usgs_main + image.find('img', class_='wide-image')['src']
+
+        sphere['title'] = title
+        sphere['img_url'] = img_url
     
+        hemisphere_image_urls.append(sphere)
+    
+
+
+# Adding all variables needed to a dictionary
+    mars = {}
+    mars['news_title'] = news_title
+    mars['news_p'] = news_p 
+    mars['featured_image_url'] = featured_image_url
+    mars['mars_facts_html'] = mars_facts_html
+    mars['hemisphere_image_urls'] = hemisphere_image_urls
+
+
+
     browser.quit()
     return mars
 
